33c9ddd3f04081cf31516077216a8e1accefed3f
olanmj <olanm@stockton.edu>
2017-10-31 00:39:13 -0400

    Add project files.

---
+++ BookDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
